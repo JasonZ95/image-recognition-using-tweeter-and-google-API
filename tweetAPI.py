2cc44645d355e2fb9db38b5cd16e83f9c939fbc3
JasonZ95 <43214200+JasonZ95@users.noreply.github.com>
2018-09-19 12:27:14 -0400

    output pictures with information on it
    This program basicly use two APis: tweepy and google cloud vision, which realize the function to download images from specific user and output the information for each picture.

---
+++ tweetAPI.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ffmpeg -f image2 -r 2  -i /home/ece-student/Desktop/601hwk/img/image%04d.jpg -vcodec libx264  -t 40 test.mp4
+# using the command above to generate the video
+
+# export GOOGLE_APPLICATION_CREDENTIALS="/home/ece-student/Desktop/601hwk/googlekey.json"
+# using the googlekey to access cloud vision service
+
+import tweepy 
+import json
+import urllib.request
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+import PIL.Image as Image
+import PIL.ImageDraw as ImageDraw
+import PIL.ImageFont as ImageFont
+
+#Twitter API credentials
+consumer_key = "frxPsqLOA8itHCihffT5bcezH"
+consumer_secret = "wt9cmEoEVlbMnHWCAyPLQmCe8nuzx3fe7AVVDHD8zbqLnp6OWs"
+access_key = "1040690616462725121-dHBkVcfHJl3egcoDMFqINj8fvzjSEe"
+access_secret = "CnztumLQUezuteatlJL1gVxUposRjFEeX7gd0GxoVd3V3"
+
+font = ImageFont.truetype('LiberationSans-Regular.ttf', 60)
+
+def get_all_tweets(screen_name):
+
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    # Imports the Google Cloud client library
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
+    
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    #save most recent 200 tweets
+    alltweets.extend(new_tweets)
+
+ 
+    #write tweet objects to JSON
+    file = open('tweet.json', 'w')
+    print ("Writing tweet objects to JSON please wait...")
+    count=1
+    path='/home/ece-student/Desktop/601hwk/img/'
+    #extracting images in 200 tweets
+    for status in alltweets:
+        json.dump(status._json,file,sort_keys = True,indent = 4)
+        if 'media'in status._json['entities']:
+            urllib.request.urlretrieve(status._json['entities']['media'][0]["media_url_https"], path + 'image'+str('%04d'%count)+'.jpg')
+            
+            # The name of the image file to annotate
+            file_name = os.path.join(os.path.dirname(__file__),  path + 'image'+str('%04d'%count)+'.jpg')
+            # Loads the image into memory
+            with io.open(file_name, 'rb') as image_file:
+                content = image_file.read()
+
+            image = types.Image(content=content)
+
+            # Performs label detection on the image file
+            response = client.label_detection(image=image)
+            labels = response.label_annotations
+
+            imageFile = path + 'image'+str('%04d'%count)+'.jpg'
+            im1=Image.open(imageFile)
+            draw = ImageDraw.Draw(im1)
+            
+            description=[]
+            for label in labels:
+                description.append(label.description)
+            print (description)
+            string=','.join(description)  
+            
+            draw.text((0, 0),string,(0,0,255),font=font)
+            draw = ImageDraw.Draw(im1)
+            im1.save(path + 'image'+str('%04d'%count)+'.jpg')
+
+
+
+            
+            
+            count= count+1
+        else:
+            pass
+            
+    #close the file
+    print ("Done")
+    print ('There are'+str(count-1)+'images in 200 tweets')
+
+   
+    file.close()
+
+if __name__ == '__main__':
+    #pass in the username of the account you want to download
+    get_all_tweets("@celtics")
+
+
+
+
